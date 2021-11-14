87b51795bbb8ae97e4c92b5e7be2bf929f3ac1ad
WEEEEEEEEEEEEEEEEEEEEEEE <thanussian@gmail.com>
2021-11-14 15:27:02 -0500

    finished header and skills page

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -106,8 +106,37 @@ class Portfolio extends React.Component {
           </Container>
         </section>
 
-        <section className="projects">
+        <br />
+        <br />
 
+        <section className="projects">
+          <Container>
+            <h1 className="text-center"> Projects </h1>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+              <Row>
+                <Col sm={3}>
+                  <Nav variant="pills" className="flex-column">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Col>
+                <Col sm={9}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                      <p> First </p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p> Second </p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+          </Container>
         </section>
 
         <section className="experience">
