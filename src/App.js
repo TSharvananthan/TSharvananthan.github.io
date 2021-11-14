557920f15e6efc3f0b38036b393e56e7af82df05
WEEEEEEEEEEEEEEEEEEEEEEE <thanussian@gmail.com>
2021-11-14 15:26:36 -0500

    connected porfolio with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route,  Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import Portfolio from "./portfolio/Portfolio.js";
+import Home from "./home/Home.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
