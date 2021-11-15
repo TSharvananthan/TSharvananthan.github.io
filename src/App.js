7366d5f3fbd5dbfe03dc367741d5e68c6177a193
WEEEEEEEEEEEEEEEEEEEEEEE <thanussian@gmail.com>
2021-11-15 06:14:31 -0500

    small fixes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route,  Link } from "react-router-dom";
 
-import Portfolio from "./portfolio/Portfolio.js";
-import Home from "./home/Home.js";
+import PortfolioApp from "./portfolio/Portfolio.js";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Portfolio />} />
+          <Route exact path="/" element={<PortfolioApp />} />
         </Routes>
       </BrowserRouter>
     )
